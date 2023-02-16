8aebcc07484f872b2a1ced615d091269344b76d5
rosendo <rosendc30@gmail.com>
2023-02-16 19:44:40 -0300

    javascript scroll

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+const menuLinks = document.querySelectorAll('.menu a[href^="#"]');
+
+function getDistanceFromTop(element) {
+  const id = element.getAttribute("href");
+  return document.querySelector(id).offsetTop;
+}
+
+function nativeScroll(distanceFromTheTop){
+window.scroll({
+  top: distanceFromTheTop,
+  behavior: "smooth",
+})
+}
+
+function scrollToSection(event) {
+  event.preventDefault();  
+  const distanceFromTheTop = getDistanceFromTop(event.target) - 90;
+  nativeScroll(distanceFromTheTop);
+}
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", scrollToSection);
+});
+
 const btnMobileOpen = document.querySelector("#btn-mobile-open");
 const btnMobileClose = document.querySelector("#btn-mobile-close");
 const ul = document.querySelector("ul");
@@ -10,7 +34,7 @@ const li4 = document.querySelector("#li4");
 btnMobileOpen.addEventListener("click", () => {
   ul.classList.toggle("active");
   btnMobileClose.classList.remove("hiden");
-  btnMobileOpen.classList.add("hiden");  
+  btnMobileOpen.classList.add("hiden");
 });
 btnMobileClose.addEventListener("click", () => {
   ul.classList.toggle("active");
