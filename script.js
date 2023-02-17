1224b759ebc3a280c8c79b9532e65d576159c93c
rosendo <rosendc30@gmail.com>
2023-02-17 10:38:18 -0300

    commit menu

--- script.js
+++ script.js
@@ -1,27 +1,4 @@
 const menuLinks = document.querySelectorAll('.menu a[href^="#"]');
-
-function getDistanceFromTop(element) {
-  const id = element.getAttribute("href");
-  return document.querySelector(id).offsetTop;
-}
-
-function nativeScroll(distanceFromTheTop){
-window.scroll({
-  top: distanceFromTheTop,
-  behavior: "smooth",
-})
-}
-
-function scrollToSection(event) {
-  event.preventDefault();  
-  const distanceFromTheTop = getDistanceFromTop(event.target) - 90;
-  nativeScroll(distanceFromTheTop);
-}
-
-menuLinks.forEach((link) => {
-  link.addEventListener("click", scrollToSection);
-});
-
 const btnMobileOpen = document.querySelector("#btn-mobile-open");
 const btnMobileClose = document.querySelector("#btn-mobile-close");
 const ul = document.querySelector("ul");
@@ -30,6 +7,34 @@ const li1 = document.querySelector("#li1");
 const li2 = document.querySelector("#li2");
 const li3 = document.querySelector("#li3");
 const li4 = document.querySelector("#li4");
+
+function getDistanceFromTop(element) {
+  const id = element.getAttribute("href");
+  return document.querySelector(id).offsetTop;
+}
+
+function nativeScroll(distanceFromTheTop) {
+  window.scroll({
+    top: distanceFromTheTop,
+    behavior: "smooth",
+  });
+}
+
+function scrollToSection(event) {
+  event.preventDefault();
+  const distanceFromTheTop = getDistanceFromTop(event.target) - 90;
+  nativeScroll(distanceFromTheTop);
+}
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", scrollToSection);
+});
+
+function openCloseMenu() {
+  ul.classList.remove("active");
+  btnMobileClose.classList.toggle("hiden");
+  btnMobileOpen.classList.toggle("hiden");
+}
 
 btnMobileOpen.addEventListener("click", () => {
   ul.classList.toggle("active");
@@ -41,28 +46,34 @@ btnMobileClose.addEventListener("click", () => {
   btnMobileClose.classList.add("hiden");
   btnMobileOpen.classList.remove("hiden");
 });
-topo.addEventListener('click', ()=>{
-  ul.classList.remove("active");
-  btnMobileClose.classList.toggle("hiden");
-  btnMobileOpen.classList.toggle("hiden");
-})
-li1.addEventListener('click', ()=>{
-  ul.classList.remove("active");
-  btnMobileClose.classList.toggle("hiden");
-  btnMobileOpen.classList.toggle("hiden");
-})
-li2.addEventListener('click', ()=>{
-  ul.classList.remove("active");
-  btnMobileClose.classList.toggle("hiden");
-  btnMobileOpen.classList.toggle("hiden");
-})
-li3.addEventListener('click', ()=>{
-  ul.classList.remove("active");
-  btnMobileClose.classList.toggle("hiden");
-  btnMobileOpen.classList.toggle("hiden");
-})
-li4.addEventListener('click', ()=>{
-  ul.classList.remove("active");
-  btnMobileClose.classList.toggle("hiden");
-  btnMobileOpen.classList.toggle("hiden");
-})
+topo.addEventListener("click", () => {
+  openCloseMenu();
+});
+li1.addEventListener("click", () => {
+  openCloseMenu();
+  li1.classList.add("cliked");
+  li2.classList.remove("cliked");
+  li3.classList.remove("cliked");
+  li4.classList.remove("cliked");
+});
+li2.addEventListener("click", () => {
+  openCloseMenu();
+  li1.classList.remove("cliked");
+  li2.classList.add("cliked");
+  li3.classList.remove("cliked");
+  li4.classList.remove("cliked");
+});
+li3.addEventListener("click", () => {
+  openCloseMenu();
+  li1.classList.remove("cliked");
+  li2.classList.remove("cliked");
+  li3.classList.add("cliked");
+  li4.classList.remove("cliked");
+});
+li4.addEventListener("click", () => {
+  openCloseMenu();
+  li1.classList.remove("cliked");
+  li2.classList.remove("cliked");
+  li3.classList.remove("cliked");
+  li4.classList.add("cliked");
+});
